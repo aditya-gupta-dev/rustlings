3f0536efb3fd7c307ee48b2fcdb908ff1dcb0687
Aditya Gupta <adityagupta2087@gmail.com>
2025-12-28 03:41:41 +0530

    lecture 2: completed

--- lect-2/src/main.rs
+++ lect-2/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    let 
+    let line: &str = "aditya is a noice boi😀"; // this takes 6 bytes 
+
+    let _: usize = line.len(); // no. of bytes used
+
+    // for indexing a str since normal [0] is not possible 
+    let _: &str = &line[0..2];
+    // let slice: &str = &line[0..3]; // this throws error  
+
+    // iterating over a string 
+    for index in  line.as_bytes().iter() { 
+        dbg!(index);
+    }    
+
+    for index in  line.chars() { 
+        dbg!(index);
+    }
+
+    let mut state: String = String::new();
+
+    state += "time package";
+    println!("{}", state);
 }
